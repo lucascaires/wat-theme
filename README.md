b95f25fe8bab2e84d4eb17681335f1a14d4ecb90
Lucas Caires <lucas.cairesd@gmail.com>
2020-11-05 00:42:26 -0300

    remove alpine code

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # WAT
 WordPress + Alpine.JS + TailwindCSS starter theme
-<div x-data="{ open: false }">
-    <button @click="open = true">Open Dropdown</button>
-
-    <ul
-        x-show="open"
-        @click.away="open = false"
-    >
-        Dropdown Body
-    </ul>
-</div>
--- index.php
+++ index.php
@@ -4,16 +4,6 @@
   <h1 class="uppercase font-light text-gray-800 bg-white p-20 text-2xl rounded-lg">
     WordPress + Alpine.js + Tailwind
   </h1>
-  <div x-data="{ open: false }">
-    <button @click="open = true">Open Dropdown</button>
-
-    <ul
-        x-show="open"
-        @click.away="open = false"
-    >
-        Dropdown Body
-    </ul>
-</div>
 </section>
 
 <?php get_footer(); ?>
