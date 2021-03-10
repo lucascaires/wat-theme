30023cdf67870bc556e49a13f5e975b3d519e632
Lucas Caires <lucas.cairesd@gmail.com>
2021-03-10 10:13:08 -0500

    insert some functions

--- functions.php
+++ functions.php
@@ -26,3 +26,35 @@ function add_assets() {
 	wp_enqueue_script( 'wat-script', get_template_directory_uri() . '/assets/app.js' );
 }
 add_action( 'wp_enqueue_scripts', 'add_assets' );
+
+
+// add some theme support
+add_theme_support( 'custom-logo' );
+add_theme_support( 'post-thumbnails' );
+
+//register menu
+register_nav_menu( 'primary',  'Main');
+
+
+
+// custom get menu function
+if ( ! function_exists( 'get_menu_by_location' ) ) {
+  function get_menu_by_location( $theme_location, $args = array() ) {
+    $theme_locations = get_nav_menu_locations();
+    $menu_obj = get_term( $theme_locations[ $theme_location ], 'nav_menu' );
+    return ( $menu_obj )  
+      ? wp_get_nav_menu_items( $menu_obj->term_id, $args )
+      : $menu_obj;
+  }
+}
+
+// custom get logo src
+if (! function_exists('get_custom_logo_src')) {
+    function get_custom_logo_src($size = 'full')
+    {
+        $logo = get_theme_mod('custom_logo');
+        list($src, $width, $height) = wp_get_attachment_image_src($logo, $size);
+        return $src;
+    }
+}
+
