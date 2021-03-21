f453f3d20e6dc63da1b6909f18edc36f9436ca8f
Lucas Caires <lucas.cairesd@gmail.com>
2021-03-21 16:12:16 -0400

    adding tailwind jit

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,8 @@
-require('laravel-mix-purgecss');
 const mix = require('laravel-mix');
-
 
 mix
   .setPublicPath('assets')
   .postCss('src/app.css', 'assets', [
-    require('tailwindcss'),
+    require("@tailwindcss/jit"),
   ])
   .js('src/app.js', 'assets')
-
-
-if (mix.inProduction()) {
-  mix.purgeCss().version()
-}
